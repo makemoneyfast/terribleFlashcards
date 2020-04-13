4d7c71c9e16da63736bcff8ed2e0e5449124f376
Alex Murray <make.money.fast@gmail.com>
2020-04-13 23:04:24 +0900

    Fixing a nasty bug

--- src/redux/assetsDuck.ts
+++ src/redux/assetsDuck.ts
@@ -154,8 +154,8 @@ export default function reducer(
                         meaning: action.payload.meaning,
                         notes: action.payload.hint,
                         tags: action.payload.tags.slice().sort(),
-                        onyomi: action.payload.kunyomi,
-                        kunyomi: action.payload.onyomi,
+                        kunyomi: action.payload.kunyomi,
+                        onyomi: action.payload.onyomi,
                         retest: false
                     }
                 }
